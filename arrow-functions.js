c88cd90f5ff94e5c8f2190728f78c89f7746631a
Stephen <sdarcy29@hotmail.com>
2021-12-09 20:30:38 +0000

    Initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,46 @@
 
 // Vanilla JavaScript Function
 
-// Arrow Function With Parameters
+// function addtwoNumbers(a, b) {
+//     return a + b
+// }
+// result = addtwoNumbers(3, 4)
+
+// console.log(result)
+
+// Arrow Function With Parameters used in more complex functions
+const addtwoNumbers = (a, b) => {
+    return a + b
+}
+result = addtwoNumbers(3, 4)
+
+console.log(result)
+
 
 // Single Line Arrow Function With Parameters
 
+const addtwoNum2 = (a, b) => a + b;
+result2 = addtwoNum2(23, 4)
+
+console.log(result2)
+
+
 // Implicit Returns
 
+const saySomething = message => console.log(message)
+saySomething('Hello')
+
+// declaring a function with no parameters needs a set of empty parenthesis
+
+const sayHello = () => console.log('hello')
+sayHello();
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+    This is a multi line string
+    </p>`
+)
+
+console.log(returnMultipleLines())
